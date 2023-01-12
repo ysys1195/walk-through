b7f24d3795c33ed1076da793d1e7589fe6c5fd98
ysys1195 <73346157+ysys1195@users.noreply.github.com>
2023-01-12 23:40:05 +0900

    first commit

---
+++ Assets/Scripts/PlayerKey.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerKey : MonoBehaviour
+{
+    public float speed = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        transform.Translate(x * speed, 0.0f, z * speed);
+    }
+}
